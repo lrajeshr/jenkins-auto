f24ab87d2af8d86fd965f08e70ee92b8a034ed75
lrajeshr <rajeshlr.info@gmail.com>
2025-03-15 22:30:59 +0530

    new module added by developer 2

---
+++ test_module.py
@@ -0,0 +1,28 @@
+import pytest
+
+# smtpserver = "smtp.gmail.com"
+
+def test_ehlo(smtp_connection):
+    response, msg = smtp_connection.ehlo()
+    assert response == 250
+    assert b"smtp.gmail.com" in msg
+    assert 0  # for demo purposes
+
+
+def test_noop(smtp_connection):
+    response, msg = smtp_connection.noop()
+    assert response == 250
+    assert 0  # for demo purposes
+
+class App:
+    def __init__(self, smtp_connection):
+        self.smtp_connection = smtp_connection
+
+
+@pytest.fixture(scope="module")
+def app(smtp_connection):
+    return App(smtp_connection)
+
+@pytest.mark.new
+def test_smtp_connection_exists(app):
+    assert app.smtp_connection
